386bd314bd298a9b0579afb2fe0e0065d6401b6f
nickzaf95 <nicholaszafiriou@gmail.com>
2020-02-18 12:18:06 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    #Dog Class
+    def name
+        @name
+    end
+
+    def name=(new)
+        @name = new
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(new_breed)
+        @breed = new_breed
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+    #Person class
+
+    def name
+        @name
+    end
+
+    def name=(new)
+        @name = new
+    end
+
+    def job
+        @job
+    end
+
+    def job=(new_job)
+        @job = new_job
+    end
+
+end
